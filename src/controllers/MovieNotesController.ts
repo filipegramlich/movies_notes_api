6dbd63bc29aeb82e43f8807672fbc0546042e69d
filipegramlich <filipe.ogramlich@gmail.com>
2023-03-20 16:16:10 -0300

    create_movies_notes +  tags
    It's a update in the create movies notes method. The movie tags was added.

--- src/controllers/MovieNotesController.ts
+++ src/controllers/MovieNotesController.ts
@@ -1,25 +1,46 @@
 
 import { Request, Response } from 'express';
 import { connection } from "../database/knex";
+import { AppError } from '../Utils/AppError';
 
 export class MovieNotesController {
 
-    async create(request:Request, response:Response){
+    async create(request: Request, response: Response) {
 
         const { user_id } = request.params;
 
-        const { title, description, rating } = request.body;
+        const { title, description, rating, tags } = request.body;
 
-        await connection('movie_notes').insert({
+        if (rating > 5) {
+            throw new AppError({ message: 'O rating para o filme deve ser de 0 até 5!', statusCode: 400 })
+        }
+
+        const [note_id] = await connection('movie_notes').insert({
 
             title,
             description,
             rating,
             user_id
-    
-        })
+
+        });
+
+        const tagsOfMovie = await tags.map((name: any) => {
+
+            return { 
+                note_id, 
+                name, 
+                user_id 
+            }
+
+        });
+
+        await connection('movie_tags').insert(tagsOfMovie);
 
         return response.json()
+    }
+
+    async update(request: Request, response: Response) {
+
     }
 
 }
